db22ad393a63b939455c00b45808d43343cbd5f6
luccasmeyer <81263064+luccasmeyer@users.noreply.github.com>
2023-06-05 19:48:43 -0300

    Mudança na tela

--- MercadoPY/testededados.py
+++ MercadoPY/testededados.py
@@ -16,24 +16,27 @@ def cotacoes():
     bitcoin = req_dic['BTCBRL']['bid']
 
     texto = f'''
-        dolar: {dolar}
-        euro: {euro}
+        dolar: {dolar} 
+        euro: {euro} 
         bitcoin: {bitcoin}'''
-    
     texto_cotacoes["text"] = texto
 
 tela = Tk()
-
+tela.geometry("300x300")
 tela.title("COTAÇÃO ATUAL")
 
-texto_cotacoes = Label(tela, text="TELA DE COTAÇÕES")
-texto_cotacoes.grid(column=0, row=0)
 
-botao = Button(tela, text="COTAÇÕES", command=cotacoes)
-botao.grid(column=0, row=1)
+texto_cotacoes = Label(tela, text="TELA DE COTAÇÕES")
+texto_cotacoes.grid(column=0, row=0, padx=75, pady=10)
+
+botao = Button(tela, text="COTAÇÕES DAS MOEDAS", command=cotacoes)
+botao.grid(column=0, row=1, padx=75, pady=10)
+
 
 texto_cotacoes = Label(tela, text="")
-texto_cotacoes.grid(column=0, row=2)
+texto_cotacoes.grid(column=0, row=2, padx=75, pady=10)
 
 
 tela.mainloop()
+
+#tentar deixar o label das moedas com o mesmo numero de caracteres
